f36b5d1d5126df5fa0735c524508f1576dd6b731
Abu Naim <abunaim@Abus-Air-2.lan>
2024-05-05 22:48:31 +0600

    show data from cart

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -6,24 +6,27 @@ import "./Cart.css";
 
 const Cart = ({ cart }) => {
   //   const { cart } = props;
- 
+
   let total = 0;
   let shipping = 0;
+  let quantity = 0;
   for (const p of cart) {
-    total = total + p.price;
-    shipping = shipping + p.shipping;
+    p.quantity = p.quantity || 1;
+    total = total + p.price * p.quantity;
+    shipping = shipping + p.shipping * p.quantity;
+    quantity = quantity + p.quantity;
   }
   const tax = (total * 5) / 100;
   const grandTotal = total + tax + shipping;
 
   return (
-    <div className='cart-container'>
-        <h2>Order Summary</h2>
-        <p>Selected Items: {cart.length}</p>
-        <p>Total Price: ${total}</p>
-        <p>Shipping: ${shipping}</p>
-        <p>Tax: ${tax}</p>
-        <h4>Grand Total: ${grandTotal.toFixed(2)}</h4>
+    <div className="cart-container">
+      <h2>Order Summary</h2>
+      <p>Selected Items: {quantity}</p>
+      <p>Total Price: ${total}</p>
+      <p>Shipping: ${shipping}</p>
+      <p>Tax: ${tax}</p>
+      <h4>Grand Total: ${grandTotal.toFixed(2)}</h4>
     </div>
   );
 };
--- src/components/Shop/Shop.jsx
+++ src/components/Shop/Shop.jsx
@@ -33,7 +33,20 @@ const Shop = () => {
     },[products])
 
     const handleAddToCart = (product) =>{
-        const newCart = [...cart, product]
+        let newCart = []
+        // const newCart = [...cart, product]
+        // if product doesn't exist in the cart, then set the quantity = 1
+        // if exist then update by 1
+        const exist = cart.find(pd => pd.id === product.id)
+        if (!exist){
+            product.quantity = 1;
+            newCart = [...cart, product]
+        }
+        else{
+            exist.quantity = exist.quantity + 1;
+            const remaining = cart.filter(pd=> pd.id != product.id)
+            newCart = [...remaining, exist]
+        }
         setCart(newCart)
         addToDb(product.id)
     }
